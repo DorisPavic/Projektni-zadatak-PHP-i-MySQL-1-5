a3bcaa4ed8f5b284d0e4b17ad28fb4b5e44aff44
DorisPavic <92396471+DorisPavic@users.noreply.github.com>
2021-11-28 14:58:31 +0100

    Add files via upload

---
+++ 3. Projektni zadatak - PHP i MySQL/loviste.php
@@ -0,0 +1,52 @@
+<?php 
+	print '
+		<div class="welcome"> <h1>Welcome to Lovište!</h1></div>
+	</header>
+	<main>
+      <div id="loviste">
+         <figure id="loviste">
+            <img src="gallery/l6.jpg">
+            <figcaption><a href="gallery.php">View gallery</a></figcaption>
+         </figure>
+      </div>
+
+      <h1>Why Lovište?</h1>
+
+      <p>Lovište is a small place located in one of the most beautiful bays on the Pelješac peninsula. It is the most
+         remote, but also the sunniest place on the Pelješac peninsula. The area of Lovište includes about 4 km of
+         coastline with numerous bays and natural beaches. </p>
+         <br>
+      <p>The climate in Loviste is ideal for bathing tourism. The main bay always has warmer sea, and the number of
+         hours of sunshine per year exceeds 3000. What attracts tourists to Loviste the most are the natural beauty,
+         special gastronomic offer and unique feeling of relaxation, so many long-term visitors call Lovište - Oasis of
+         Peace. </p>
+         <br>
+      <p>The bays near Lovište will delight you with their beautiful and almost untouched pebble beaches, and the
+         residents will provide you with ideal private accommodation. Lovište is an ideal starting point for trips to
+         the islands of Korčula, Mljet and the city of Dubrovnik. Book your accommodation in Lovište and explore other
+         beauties of Riviera Pelješac. </p>
+
+
+      <nav>
+         <ul>
+            <li><a href="https://github.com/DorisPavic?tab=repositories"><img src="slike/github.png" title="Github"
+                     alt="Github" style="width:30px;height:30px"></a>
+            <li><a href="https://www.facebook.com/visitloviste"><img src="slike/face.png" alt="Facebook"
+                     title="Facebook" style="width:30px;height:30px" /></a></li>
+            <li><a href="https://www.instagram.com/loviste/?hl=hr"><img src="slike/instagram.jpg" alt="Instagram"
+                     title="Instagram" style="width:30px;height:30px" /></a></li>
+         </ul>
+      </nav>
+      
+      <footer>
+         <hr>
+         <p>Contact us:</br>
+            Tel. 091/234-5678 </br>
+            E-mail: dpavic@tvz.hr</br>
+            Address: Lovište bb </p>
+         <hr>
+         Copyright &copy; 2021 Doris Pavić. <a href="https://github.com/DorisPavic?tab=repositories"><img
+               src="slike/github.png" title="Github" alt="Github" style="width:30px;width:15px"></a></p>
+         </br>
+      </footer>';
+?>       
